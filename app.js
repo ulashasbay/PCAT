d7427aa49730e5862f40c754329caeb6b64b534d
Ulaş Hasbay <ulashasbay@gmail.com>
2021-11-15 01:02:57 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+//  middlewares
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const photo = {
-        id: 1,
-        name: "Photo Name",
-        description: "Photo Description"
-    };
-    res.send(photo);
+    
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 
