8cc60d1f0bb71883afa11d1b8e22454cd31efe3e
Ulaş Hasbay <ulashasbay@gmail.com>
2021-11-23 17:57:49 +0300

    delete button created

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}));
 
 // Routes
 app.get('/', async (req, res) => {
@@ -85,6 +87,15 @@ app.put('/photos/:id', async (req, res) => {
     photo.description = req.body.description
     photo.save();
     res.redirect(`/photos/${req.params.id}`);
+});
+
+// Delete
+app.delete('/photos/:id', async (req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id });
+    let deletedImage = __dirname + '/public' + photo.image;
+    fs.unlinkSync(deletedImage);
+    await Photo.findByIdAndRemove(req.params.id);
+    res.redirect('/');
 });
 
 
