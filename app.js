ae9e0bf4bc647eee2add1db50aea58b9f359d282
Ulaş Hasbay <ulashasbay@gmail.com>
2021-11-23 18:52:54 +0300

    MVC - controllers folder created

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo');
-const fs = require('fs');
-
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController');
 
 
 const app = express();
@@ -27,76 +25,24 @@ app.use(methodOverride('_method', {
 }));
 
 // Routes
-app.get('/', async (req, res) => {
-    const photos = await Photo.find({}).sort('-dateCreated');
-    res.render('index', {
-        photos
-    });
-});
-
+app.get('/', photoController.getAllPhotos);
 // Tekli photo sayfaları
-app.get('/photos/:id', async (req, res) => {
-    const photo = await Photo.findById(req.params.id);
-    res.render('photo', {
-        photo
-    });
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/add', (req, res) => {
-    res.render('add');
-});
-
+app.get('/photos/:id', photoController.getPhoto);
 // Upload edilecek image çin önce eğer yok ise public/uploads klasörü oluşturyoruz. Sonrasında image i oraya taşıyıp adresini mongodb ye yazıyoruz
-app.post('/photos', (req, res) => {
-    // await Photo.create(req.body);
-    // res.redirect('/');
-    const uploadDir = 'public/uploads';
-
-    if (!fs.existsSync(uploadDir)) {
-        fs.mkdirSync(uploadDir)
-    };
-
-    let uploadedImage = req.files.image;
-    let uploadedPath = __dirname + '/public/uploads/' + uploadedImage.name;
-
-    uploadedImage.mv(uploadedPath, async () => {
-        await Photo.create({
-            ...req.body,
-            image: '/uploads/' + uploadedImage.name
-        });
-        res.redirect('/');
-    });   
-});
-
-// Edit page
-app.get('/photos/edit/:id', async (req, res) => {
-    const photo = await Photo.findOne({ _id : req.params.id });
-    res.render('edit', {
-        photo
-    });
-});
-
+app.post('/photos', photoController.createPhoto);
 // Update 
-app.put('/photos/:id', async (req, res) => {
-    const photo = await Photo.findOne({ _id: req.params.id });
-    photo.title = req.body.title
-    photo.description = req.body.description
-    photo.save();
-    res.redirect(`/photos/${req.params.id}`);
-});
-
+app.put('/photos/:id', photoController.updatePhoto);
 // Delete
-app.delete('/photos/:id', async (req, res) => {
-    const photo = await Photo.findOne({ _id: req.params.id });
-    let deletedImage = __dirname + '/public' + photo.image;
-    fs.unlinkSync(deletedImage);
-    await Photo.findByIdAndRemove(req.params.id);
-    res.redirect('/');
-});
+app.delete('/photos/:id', photoController.deletePhoto);
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+// Edit page
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+
+
+
 
 
 const port = 3000;
